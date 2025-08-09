911edef45610895b9a8809df5ea4ac2f5dbe2306
danadum <danadum.empire@gmail.com>
2025-08-09 19:30:59 +0200

    v2.2.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="pygge",
-    version="2.2.5",
+    version="2.2.6",
     description="A Python library for interacting with GoodGame Empire API.",
     author="danadum",
     author_email="danadum.empire@gmail.com",
